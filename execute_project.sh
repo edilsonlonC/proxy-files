91033eae69d8e6c9c04d7b30363f621b923e7431
edilson <eddylson.londonio@gmail.com>
2020-09-19 20:04:02 -0500

    script for servers

---
+++ execute_project.sh
@@ -0,0 +1,7 @@
+gnome-terminal -e "zsh -c 'python proxy/main.py 5555; zsh;'"
+
+gnome-terminal -e "zsh -c 'python server/server.py 5555; zsh;'"
+
+gnome-terminal -e "zsh -c 'python server/server.py 5557; zsh;'"
+
+gnome-terminal -e "zsh -c 'python server/server.py 5558; zsh;'"
---
+++ server/execute_server.sh
@@ -0,0 +1,5 @@
+gnome-terminal -e "zsh -c 'python server.py 5555; zsh;'"
+
+gnome-terminal -e "zsh -c 'python server.py 5557; zsh;'"
+
+gnome-terminal -e "zsh -c 'python server.py 5558; zsh;'"
